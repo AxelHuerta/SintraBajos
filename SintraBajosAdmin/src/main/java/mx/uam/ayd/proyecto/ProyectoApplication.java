22ecd6d25bd152adedba0c5daf7a67ed591f8a1a
Axel Huerta <102191829+AxelHuerta@users.noreply.github.com>
2023-04-04 14:48:03 -0600

    GUI Admin

---
+++ SintraBajosAdmin/src/main/java/mx/uam/ayd/proyecto/ProyectoApplication.java
@@ -0,0 +1,58 @@
+package mx.uam.ayd.proyecto;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import mx.uam.ayd.proyecto.presentacion.principalAdministrador.ControlPrincipal;
+
+/**
+ * 
+ * Clase principal que arranca la aplicación 
+ * construida usando el principio de 
+ * inversión de control
+ * 
+ * 
+ * @author Sintra Bajos
+ * Este cambio es en la rama de trabajo
+ */
+@SpringBootApplication
+public class ProyectoApplication {
+
+	@Autowired
+	ControlPrincipal controlPrincipal;
+	
+	
+	/**
+	 * 
+	 * Método principal
+	 *
+	 * @params args argumentos de la línea de comando
+	 * 
+	 */
+	public static void main(String[] args) {
+		
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(ProyectoApplication.class);
+
+		builder.headless(false);
+
+		builder.run(args);
+	}
+
+	/**
+	 * Metodo que arranca la aplicacion
+	 * inicializa la bd y arranca el controlador
+	 * otro comentario
+	 */
+	@PostConstruct
+	public void inicia() {
+				
+		controlPrincipal.inicia();
+	}
+	
+	
+
+
+}
---
+++ SintraBajosAdmin/src/main/java/mx/uam/ayd/proyecto/presentacion/listarUsuarios/ControlListarUsuarios.java
@@ -0,0 +1,72 @@
+package mx.uam.ayd.proyecto.presentacion.listarUsuarios;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+import mx.uam.ayd.proyecto.negocio.ServicioUsuario;
+import mx.uam.ayd.proyecto.negocio.modelo.Usuario;
+
+@Slf4j
+@Component
+public class ControlListarUsuarios {
+
+  @Autowired
+  private ServicioUsuario servicioUsuario;
+
+  @Autowired
+  private ventanaDiagnosticoAdmin ventana;
+
+  @Autowired
+  private ventanaListaUsuarios ventana2;
+
+  public void inicia() {
+    ventana2.muestra(this);
+  }
+
+  public void iniciaUsuarios() {
+    List<Usuario> pacientes = servicioUsuario.recuperaUsuarios();
+    for (Usuario paciente : pacientes) {
+      log.info("paciente: " + paciente);
+    }
+  }
+
+  ////////////////////////////////////////////////////////
+  // el metodo esta dos veces descrito de forma diferente
+  // una aquí y la otra en el control principla
+  ////////////////////////////////////////////////////////
+  public List<Usuario> obtenUsuarios() {
+    List<Usuario> pacientes = servicioUsuario.recuperaUsuarios();
+    return pacientes;
+  }
+  //Metodo usado para obtener al paciente seleccionado
+  public void pacienteActual(Usuario usuario) {
+    ventana.muestra(this, usuario); // Muestra la ventana y muestra al paciente obtenido
+  }
+// Se obtiene el nuevo usuario
+  public void recuperaNuevo(Usuario usuario) {
+    servicioUsuario.nuevo(usuario);
+    ventana.muestraMensaje();
+    ventana2.muestraNuevo();
+  }
+
+  ///////////////////////////////////////
+  // Esta en el control principal
+  ///////////////////////////////////////
+  // Metodo para obtener al paciente
+  public void recuperaUsuario(Usuario paciente) {
+    ventana2.termina();
+    pacienteActual(paciente);
+   
+  }
+
+
+  /*
+   *  Termina la historia de usuario
+   */
+  public void termina() {
+    ventana2.setVisible(false);
+  }
+}
